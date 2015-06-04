e1633f65a3f27827117a08d5d9bcca05f50ad924
Sergey <vilgodskiy@gmail.com>
2015-06-04 16:48:35 +0300

    second commit

---
+++ src/main/java/ru/lenoblgis/sergey/test/security/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package ru.lenoblgis.sergey.test.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
